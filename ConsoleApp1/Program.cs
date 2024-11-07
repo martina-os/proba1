3fd7cca014abff85048119e6105a9468e7a06402
Martina Ostović <martina12.mo@gmail.com>
2024-11-07 20:17:52 +0100

    cherry

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("Pozdrav iz aplikacije od Martina Ostović, jmbag: 1311029184 ");
+=======
+Console.WriteLine("Pozdrav iz aplikacije od Martina Ostović");
+>>>>>>> ce52868 (dodavanje konzolne app)
